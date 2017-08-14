b01d123ba14f563f222e805a86df48d3c476c317
sssubham90 <yoyosssubham@gmail.com>
2017-08-14 23:24:54 +0530

    v1.0.0

--- app/src/main/java/com/riantservices/riant/TripsActivity.java
+++ app/src/main/java/com/riantservices/riant/TripsActivity.java
@@ -10,7 +10,6 @@ import android.app.FragmentManager;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
